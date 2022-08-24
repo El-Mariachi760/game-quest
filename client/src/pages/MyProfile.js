a5b9a5dde6c09b0c74b3412582938a47d012d481
Laura Jazmin Anderson <jazminandersonxo@gmail.com>
2022-08-24 13:55:24 -0700

    profile pic add

--- client/src/pages/MyProfile.js
+++ client/src/pages/MyProfile.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import FriendList from '../components/FriendList';
 import Nav from '../components/Nav';
+import ProfilePic from '../assets/ProfilePic'
 
 function MyProfile() {
 
     return (
       <div>
         
-        
           <Nav />
           <h1>MyProfile</h1>
+          <ProfilePic/>
           <FriendList />
       </div>
     );
