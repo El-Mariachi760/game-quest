b3511e46ac11ff9b47ea2aeb6d1301cd3196616d
Laura Jazmin Anderson <jazminandersonxo@gmail.com>
2022-08-24 18:43:50 -0700

    profile edit

---
+++ client/src/components/RSVPEvents/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RSVPEvents () {
+    return (
+        <div>
+            <h1>RSVP Events</h1>
+        </div>
+    )
+}
+
+export default RSVPEvents
--- client/src/pages/MyProfile.js
+++ client/src/pages/MyProfile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FriendList from '../components/FriendList';
 import Nav from '../components/Nav';
-import ProfilePic from '../assets/Profile-pic.jpg'
+import RSVPEvents from '../components/RSVPEvents';
 import ProfileBio from '../components/ProfileBio'
 
 function MyProfile() {
@@ -14,7 +14,7 @@ function MyProfile() {
 
           <h1 className='page-title'>My Profile</h1>
         <div className='profilepic'>
-        <ProfilePic/>
+        {/* <ProfilePic/> */}
         </div>
 
         <div className='Bio'>
@@ -22,6 +22,10 @@ function MyProfile() {
           {/* <ProfileBio/> */}
         </div >
           <FriendList />
+        </div>
+
+        <div>
+          <RSVPEvents/>
         </div>
 
       </section>
