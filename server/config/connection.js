d4072dc695ac1e1d086b6b56d7542cf28a2e837d
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-08-27 19:48:54 -0700

    Mongo fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/game-quest', {});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/game-quest', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
